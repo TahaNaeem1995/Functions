53eb76622be53633b3c4cba5acf17be3d2fbf75b
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 15:51:22 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+// function halffryegg(){
+//     let cooked = 1+ 1.5 + 2;
+//     //Egg + Butter + Salt
+//     console.log(cooked);
+
+// }
+// halffryegg()//Invoking (means call the function)the function
+
+
+
+function halffryegg(): number {
+    let cooked = 1+ 1.5 + 2;
+    //Egg + Butter + Salt
+    return cooked
+
+}
+//Invoking the function
+let response: number = halffryegg()
+console.log(response)
